5493f7408a1f7ea8a74fcc4392251ae16d7ce082
Torichinski <michnik611@gmail.com>
2026-01-25 21:01:23 +0400

    Update VirtualMenegePanelWinderRS.cpp
    global struct characteristics

--- VirtualMenegePanelWinderRS.cpp
+++ VirtualMenegePanelWinderRS.cpp
@@ -19,16 +19,21 @@ string line;
 bool runProg = true;
 
 struct characteristics{
+	int num;
+	double diametr;
+	double kc;
 	double paz_leng;     
-	double wire_width_limit;      
-	double stator_height;  
-	double stator_diametr;   
-	double angle_motor_speed;  
-	double wire_diametr;   
-	int coils_quality; 
-	double winder_radius; 
-	double extra_wire_len;
-  }
+  	double wire_width_limit;      
+  	double stator_height;  
+  	double stator_diametr;   
+  	double angle_motor_speed;  
+  	double wire_diametr;   
+  	int coils_quality; 
+  	double winder_radius; 
+  	double extra_wire_len;
+  };
+
+
 
 void append_value(fstream& file, double value) {
     if (!file.is_open()){
@@ -36,6 +41,13 @@ void append_value(fstream& file, double value) {
 	return;
 }
     file << value << '\t';
+}
+
+void fifty_dashes(){
+	for(int i = 0; i <= 50; i++){
+		cout << "-";
+	}
+	cout << '\n';
 }
 
 void setConsoleFontSize(int width, int height) {
@@ -52,12 +64,15 @@ void setConsoleFontSize(int width, int height) {
 }
 
 void ConsoleSetup(){
-	SetConsoleTitle("WinderRS Menege Panel");
+	system("title WinderRS Menege Panel");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 }
 
 class panel{
+	public:
+		characteristics motor;
+
 	public:
 		int num;
 		double diametr;
@@ -79,7 +94,7 @@ class panel{
       	int coils_quality; 
       	double winder_radius; 
       	double extra_wire_len;
-    
+      	
       	//characteristics.push_back(0);
       	//characteristics.insert(characteristics.begin() + 1, paz_leng);
 		
@@ -92,10 +107,11 @@ class panel{
 		
 		void Make_Directory(){
 		    if (CreateDirectory(directoryPath.c_str(), NULL)) {
-		        cout << "WinderRS program has loaded :" << std::endl;
+		        cout << "WinderRS program has loaded " << std::endl;
+		        fifty_dashes(); 
 		    } 
 		    else{
-		    	cout << '\n' << endl;
+		    	fifty_dashes(); 
 			}
 		}
 		
@@ -198,8 +214,8 @@ class panel{
 	        	
 	        	cout << '\n' << "Write the number of the parameter you need (set 0 to skip parameter) :" << endl;
 	        	
-	        	for(int i = 1; i <= 8; ++i){
-	        	cout << i << ". ";
+	        	for(int i = 0; i <= 8; ++i){
+	        	cout << i + 1 << ". ";
 				cin >> comb[i];
 				
 				if (comb[i] != (int)comb[i] || comb[i] > 10000){
@@ -208,6 +224,27 @@ class panel{
 					
 				if (comb[i] != 0){
 					withExtraPars = true;
+					int idx = i + 1;
+						switch(idx) {
+							case 1:
+								paz_leng = comb[i];
+							case 2:
+								wire_width_limit = comb[i];
+							case 3:
+								stator_height = comb[i];
+							case 4:
+								stator_diametr = comb[i];
+							case 5:
+								angle_motor_speed = comb[i];
+							case 6:
+								wire_diametr = comb[i];
+							case 7:
+								coils_quality = comb[i];
+							case 8:
+								winder_radius = comb[i];
+							case 9:	
+								extra_wire_len = comb[i];
+						}
 				}
 			}
 			
@@ -216,12 +253,30 @@ class panel{
 				cout << comb[i] << ' ';
 			}			
 				
-	        cout << '\n' << "Next" << endl;
+	        cout << '\n' << wire_width_limit << endl;
 	        runInput = false;
+	        
+	        while(withExtraPars == true){
+				motor = {
+				.num = num,
+				.diametr = diametr, 
+				.kc = kc,
+				.paz_leng = paz_leng,
+				.wire_width_limit = wire_width_limit,
+				.stator_height = stator_height,
+				.stator_diametr = stator_diametr,
+				.angle_motor_speed = angle_motor_speed,
+				.wire_diametr = wire_diametr,
+				.coils_quality = coils_quality,
+				.winder_radius = winder_radius,
+				.extra_wire_len = extra_wire_len
+				};
+			}
 	        
 			}
 		
 			if (num, diametr, kc != 0 && runInput == false){
+				motor = {.num = num, .diametr = diametr, .kc = kc};
 			
 				this -> num = num;
 				this -> diametr = diametr;
@@ -250,7 +305,7 @@ class panel{
 	
 		}
 		
-		void load_parametrs(){ // çàãðóçêà ïàðàìåòðîâ íà âíåøíåå óñòðîéñòâî
+		void load_parametrs(){ 
 			
 		}
 };
@@ -258,11 +313,11 @@ class panel{
 int main(int argc, char** argv) {
 	
 setlocale(LC_ALL, "RUS");
-
 system("color 1F");
 
 setConsoleFontSize(12, 24);
 ConsoleSetup();
+
 	
 int num;
 double diametr, kc;	
@@ -274,4 +329,3 @@ Panel.setParametrs();
 
 return 0;
 }
-
